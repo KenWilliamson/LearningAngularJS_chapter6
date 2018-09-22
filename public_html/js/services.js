1c52fa753aed9e12cbcfa460b6a832fe0737fc1e
Ken Williamson <kwill1253@gmail.com>
2018-09-22 13:32:37 -0400

    changed link
    changed link to http://www.goblog.ulboralabs.com/GolangBlog

--- public_html/js/services.js
+++ public_html/js/services.js
@@ -6,7 +6,7 @@ var blogServices = angular.module('blogServices', ['ngResource']);
 
 blogServices.factory('BlogPost', ['$resource',
     function($resource) {
-        return $resource("http://nodeblog-micbuttoncloud.rhcloud.com/NodeBlog/blog/:id", {}, {
+        return $resource("http://www.goblog.ulboralabs.com/GolangBlog/blog/:id", {}, {
             get: {method: 'GET', cache: false, isArray: false},
             save: {method: 'POST', cache: false, isArray: false},
             update: {method: 'PUT', cache: false, isArray: false},
